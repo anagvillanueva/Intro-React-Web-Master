d21daa03e5934e91a6f46778fe7fe93f42670e0f
Ana V <anafernandagv27@gmail.com>
2026-09-01 20:41:52 -0600

    Agregando componente dentro de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,44 @@ import { useState} from 'react';
 import { UsuarioSaludo } from './components/UsuarioSaludo';
 import {Tarjeta} from './components/Tarjeta';
 import {EstadoSesion} from './components/EstadoSesion';
+import {FormularioLogin} from './components/FormularioLogin'
 
 function App() {
-  const [estado, setEstado] = useState('autenticado');
+  const [estado, setEstado] = useState('invitado');
+  const [username, setUsername] = useState('');
+
+  const handleLoginExitoso = (nombreUsuario) => {
+    setUsername(nombreUsuario);
+    setEstado('autenticado');
+  };
+
+  const handleCerrarSesion = () => {
+    setEstado('invitado');
+    setUsername('');
+  }
 
   return (
     <div className="panel-autenticacion">
       <h1>Panel de Autenticación</h1>
 
-      <Tarjeta titulo="Perfil del Usuario">
-        <EstadoSesion estado={estado} />
-        <UsuarioSaludo 
-          estaLogeado={estado === 'autenticado'} 
-          username="Ana" 
-          mensajeNoLeido={5} 
-        />
-        
-        <button
+      {estado === 'invitado' ? (
+        <FormularioLogin onLogin={handleLoginExitoso}/> ) : (
+          <Tarjeta titulo="Perfil del Usuario">
+            <EstadoSesion estado={estado} />
+            <UsuarioSaludo 
+              estaLogeado={estado === 'autenticado'} 
+              username="Ana" 
+              mensajeNoLeido={5} 
+            />
+
+          <button
           className="boton-alternar"
-          onClick={() => setEstado(estado === 'autenticado' ? 'invitado' : 'autenticado')}>
-          Alternar Estado
+          onClick={handleCerrarSesion}>
+          Cerrar Sesion Espacial 
         </button>
       </Tarjeta>
-    </div>
+        )}
+            </div>
   );
 }
 
